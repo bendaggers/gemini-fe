cfc58682d41f15f016d3f981b5e61e4f41e34d6b
bendaggers <benmichaeloracion@gmail.com>
2023-03-14 12:58:50 +0800

    Added Services & Interface

---
+++ src/app/interface/stock-data.ts
@@ -0,0 +1,6 @@
+export interface StockData {
+  stockid: number;
+  ticker: string;
+  stockname: string;
+  market: string;
+}
---
+++ src/app/interface/transaction-data.ts
@@ -0,0 +1,8 @@
+export interface TransactionData {
+  txndate: string | null;
+  order: string;
+  broker: string;
+  ticker: string;
+  quantity: number;
+  shareprice: number;
+}
